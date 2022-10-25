a5487a3c848e5bcaeb3286ba8f088223d1aa2164
Jairo Santos jr <79456331+jairojunior955@users.noreply.github.com>
2022-10-25 16:44:48 -0300

    Add files via upload

---
+++ Form1.Designer.cs
@@ -0,0 +1,75 @@
+﻿
+namespace EditorDeTexto
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textoConteudo = new System.Windows.Forms.TextBox();
+            this.botaoGrava = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // textoConteudo
+            // 
+            this.textoConteudo.Location = new System.Drawing.Point(12, 12);
+            this.textoConteudo.Multiline = true;
+            this.textoConteudo.Name = "textoConteudo";
+            this.textoConteudo.Size = new System.Drawing.Size(776, 298);
+            this.textoConteudo.TabIndex = 0;
+            // 
+            // botaoGrava
+            // 
+            this.botaoGrava.Location = new System.Drawing.Point(12, 334);
+            this.botaoGrava.Name = "botaoGrava";
+            this.botaoGrava.Size = new System.Drawing.Size(75, 23);
+            this.botaoGrava.TabIndex = 1;
+            this.botaoGrava.Text = "Gravar";
+            this.botaoGrava.UseVisualStyleBackColor = true;
+            this.botaoGrava.Click += new System.EventHandler(this.botaoGrava_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.botaoGrava);
+            this.Controls.Add(this.textoConteudo);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textoConteudo;
+        private System.Windows.Forms.Button botaoGrava;
+    }
+}
+
